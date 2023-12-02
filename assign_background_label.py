55bcf94a2ae324c94fda93325847051586d68805
Mark Ahmadi <mark-ahmadi@shaw.ca>
2023-12-02 08:26:09 -0800

    Pretty sure I fixed the issue with setting the background label in the PDE labels.
    Adding modified PDE labels with (hopefully) correctly set background labels.

--- assign_background_label.py
+++ assign_background_label.py
@@ -2,16 +2,16 @@ import rasterio
 import numpy as np
 import os
 
-def set_no_data_value(input_file, output_file, no_data_value):
+def set_no_data_value(input_file, output_file, no_data_value, band_value_to_replace):
     with rasterio.open(input_file, "r+") as src:
         src.nodata = no_data_value
         with rasterio.open(output_file, 'w',  **src.profile) as dst:
             band = src.read(1)
-            band = np.where(band==no_data_value, no_data_value, band)
+            band = np.where(band==band_value_to_replace, no_data_value, band)
             dst.write(band, 1)
                 
 if __name__ == "__main__":
     cwd = os.getcwd()
     input_path = os.path.join(cwd, "PDE_label\PDE_label_Area1.tif")
     output_path = os.path.join(cwd, "PDE_label\PDE_label_Area1_with_background.tif")
-    set_no_data_value(input_path, output_path, 15.0) 
+    set_no_data_value(input_path, output_path, 4, 3) 
