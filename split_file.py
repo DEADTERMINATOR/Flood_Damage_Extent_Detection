a3e61937036435f8bc2318a519f2dbc62ca3bb8f
Baorong Wei <71452937+wbrBarry@users.noreply.github.com>
2023-12-02 21:28:08 -0800

    Split and change tif file
    Split the TIFF file into smaller blocks and change them to PNG files.

--- split_file.py
+++ split_file.py
@@ -5,6 +5,19 @@ import numpy as np
 from PIL import Image
 import os
 
+def normalize(array):
+    """Normalize the array to 0-255 and convert to uint8."""
+    array = array.astype(np.float32)
+    min_val = array.min()
+    max_val = array.max()
+    if max_val - min_val > 1e-5:  # Ensure the denominator is not too small
+        array -= min_val
+        array /= max_val - min_val
+        array *= 255
+    else:
+        array = np.zeros(array.shape)  # Set to zero if there's no variation
+    return array.astype(np.uint8)
+
 def split_tiff(file_path, output_dir, grid_size=(16, 16)):
     with rasterio.open(file_path) as dataset:
         # Calculate the size of each split
@@ -12,8 +25,8 @@ def split_tiff(file_path, output_dir, grid_size=(16, 16)):
 
         # Creating window slices
         window_list = []
-        for j in range(grid_size[1]):  # Rows
-            for i in range(grid_size[0]):  # Columns
+        for j in range(grid_size[1]):
+            for i in range(grid_size[0]):
                 window = windows.Window(i * width, j * height, width, height)
                 window_list.append(window)
 
@@ -22,14 +35,19 @@ def split_tiff(file_path, output_dir, grid_size=(16, 16)):
             # Read the data in the window
             window_data = dataset.read(window=window)
 
-            # Convert to PIL Image for saving as PNG
-            img_data = np.moveaxis(window_data, 0, -1)  # Move channels to last dimension
-            img = Image.fromarray(img_data)
+            # Normalize the data
+            if dataset.count == 1:  # Single band
+                window_data = normalize(window_data[0])
+                img = Image.fromarray(window_data, 'L')
+            else:  # Multiband
+                normalized_data = [normalize(band) for band in window_data]
+                window_data = np.stack(normalized_data, axis=-1)
+                img = Image.fromarray(window_data, 'RGB')
 
             # Save the image
             img.save(os.path.join(output_dir, f'split_image_{idx}.png'), 'PNG')
 
 # Usage
-file_path = 'path_to_your_tiff_file.tif'
-output_dir = 'path_to_output_directory'
+file_path = 'align&split//align//nlcd_2016_impervious_l48_Area2_aligned.tif'
+output_dir = 'align&split//split//nlcd_2016_impervious_l48_Area2_split//'
 split_tiff(file_path, output_dir)
